ad3670f9ae197006510896050995ec44b2aa175a
Xeltalliv <vadim@vvv.kiev.ua>
2022-10-27 00:15:20 +0300

    Add Lab2

---
+++ Lab2/Astar/main.js
@@ -0,0 +1,227 @@
+const canvas = document.getElementById("canv");
+const ctx = canvas.getContext("2d");
+const queen = new Image(); queen.src = "../queen.png";
+
+let states = [];
+let finalState = null;
+let queenCount = 8;
+let fieldSize = 8;
+
+document.getElementById("start").addEventListener("click", async function() {
+	queenCount = Math.max(document.getElementById("queen").value | 0, 2);
+	fieldSize = Math.max(document.getElementById("board").value | 0, 2);
+	canvas.width = canvas.height = 44 + fieldSize * 32;
+
+	addRandomly();
+	let queensInitial = states[0].queens;
+	draw(queensInitial);
+	await wait(0.5);
+	document.getElementById("info").innerText = "Пошук шляху...";
+	let startTime = Date.now();
+
+	await solve();
+
+	document.getElementById("info").innerText = "Шлях знайдено за "+((Date.now() - startTime) / 1000)+" секунд";
+	states = [];
+	let action = finalState.whatIDid;
+	let log = [];
+	while(action) {
+		log.unshift([action[1], action[2], action[3]]);
+		action = action[4];
+	}
+	await playAnimation(queensInitial, log);
+});
+
+class State {
+	depth = 0;
+	hitfield = [];
+	actions = [];
+	queens = [];
+	whatIDid = null;
+	isSolution() {
+		let hitfield = this.hitfield;
+		for(let q=0; q<queenCount; q++) {
+			let x = this.queens[q][0];
+			let y = this.queens[q][1];
+			if(hitfield[y*8+x] > 1) return false;
+		}
+		return true;
+	}
+	genActions() {
+		let actions = [];
+		let hitfield = this.hitfield;
+		for(let q=0; q<queenCount; q++) {
+			let x = this.queens[q][0];
+			let y = this.queens[q][1];
+			for(let i=0; i<fieldSize; i++) {
+				if(x !== i) actions.push([hitfield[y*8+i], q, i, y]);
+			}
+			for(let i=0; i<fieldSize; i++) {
+				if(y !== i) actions.push([hitfield[i*8+x], q, x, i]);
+			}
+			let z = y-x;
+			for(let i=0; i<fieldSize; i++) {
+				if(x !== i && z+i >= 0 && z+i < fieldSize) actions.push([hitfield[(z+i)*8+i], q, i, z+i]);
+			}
+			z = y+x;
+			for(let i=0; i<fieldSize; i++) {
+				if(x !== i && z-i >= 0 && z-i < fieldSize) actions.push([hitfield[(z-i)*8+i], q, i, z-i]);
+			}
+		}
+		actions.sort((a,b) => a[0]-b[0]);
+		this.actions = actions;
+	}
+	makeChild(myIndex) {
+		let action = this.actions.shift();
+		let newState = new State();
+		newState.depth = this.depth + 1;
+		newState.whatIDid = action;
+		action.push(this.whatIDid);
+		newState.queens = copy(this.queens);
+		newState.hitfield = this.hitfield.slice();
+		addHitfield(newState.hitfield, newState.queens[action[1]][0], newState.queens[action[1]][1], -1);
+		newState.queens[action[1]][0] = action[2];
+		newState.queens[action[1]][1] = action[3];
+		addHitfield(newState.hitfield, action[2], action[3], 1);
+		if(newState.isSolution()) {
+			console.log(newState);
+			finalState = newState;
+			return true;
+		}
+		newState.genActions();
+		states.push(newState);
+		if(this.actions.length === 0) states.splice(myIndex, 1);
+	}
+}
+
+async function solve() {
+	while(true) {
+		let minIdx = null;
+		let minVal = Infinity;
+		for(let i=0; i<states.length; i++) {
+			let state = states[i];
+			let val = state.depth + state.actions[0][0];
+			if(val < minVal) {
+				minVal = val;
+				minIdx = i;
+			}
+		}
+		if(states[minIdx].makeChild(minIdx)) return true;
+		if(Math.random() < 0.01) {
+			//console.log(minVal);
+			await wait(0.001);
+		}
+	}
+}
+
+
+function draw(queens) {
+	queens = queens.slice().sort((a,b) => a[1]-b[1]);
+	ctx.clearRect(0, 0, 300, 300);
+
+	ctx.fillStyle = "black";
+	ctx.fillRect(20, 20, fieldSize*32+4, fieldSize*32+4);
+	ctx.font = "20px sans-serif";
+
+	const colors = ["white", "black"];
+	for(let y=0; y<fieldSize; y++) {
+		for(let x=0; x<fieldSize; x++) {
+			ctx.fillStyle = colors[(x+y) % 2];
+			ctx.fillRect(22+x*32, 22+y*32, 32, 32);
+		}
+	}
+	for(let q=0; q<queenCount; q++) {
+		let x = queens[q][0];
+		let y = queens[q][1];
+		ctx.drawImage(queen, 22+x*32, 22+y*32);
+		/*let l = ctx.measureText(queens[q][2]).width/2;
+		ctx.fillStyle = "#000000";
+		ctx.fillText(queens[q][2], 38-l+x*32, 46+y*32);
+		ctx.fillStyle = "#ff0000";
+		ctx.fillText(queens[q][2], 38-l+x*32, 44+y*32);*/
+	}
+	ctx.fillStyle = "black";
+	let fs32 = fieldSize*32;
+	for(let y=0; y<fieldSize; y++) {
+		ctx.fillText("12345678"[y], 4, 12+fs32-y*32);
+		ctx.fillText("12345678"[y], 31+fs32, 12+fs32-y*32);
+		ctx.fillText("ABCDEFGH"[y], 33+y*32, 14);
+		ctx.fillText("ABCDEFGH"[y], 33+y*32, 44+fs32);
+	}
+}
+
+function addRandomly() {
+	let newState = new State();
+	let queens = newState.queens;
+	let hitfield = newState.hitfield = new Array(8*8).fill(0);
+	let add = queenCount;
+	while(add > 0) {
+		let x = random(0, fieldSize-1);
+		let y = random(0, fieldSize-1);
+		let skip = false;
+		for(let q=0; q<queens.length; q++) {
+			if(queens[q][0] === x && queens[q][1] === y) skip = true;
+		}
+		if(skip) continue;
+
+		addHitfield(hitfield, x, y, 1);
+		queens.push([x, y]);
+		add--;
+	}
+	draw(queens);
+	newState.genActions();
+	states.push(newState);
+}
+
+function addHitfield(hitfield, x, y, val) {
+	hitfield[y*8+x] += val;
+	for(let i=0; i<fieldSize; i++) {
+		if(x !== i) hitfield[y*8+i] += val;
+	}
+	for(let i=0; i<fieldSize; i++) {
+		if(y !== i) hitfield[i*8+x] += val;
+	}
+	let z = y-x;
+	for(let i=0; i<fieldSize; i++) {
+		if(x !== i && z+i >= 0 && z+i < fieldSize) hitfield[(z+i)*8+i] += val;
+	}
+	z = y+x;
+	for(let i=0; i<fieldSize; i++) {
+		if(x !== i && z-i >= 0 && z-i < fieldSize) hitfield[(z-i)*8+i] += val;
+	}
+}
+
+function random(a, b) {
+	return a + Math.floor(Math.random() * (b-a+1));
+}
+
+let delay = 0.1;
+
+function copy(arr) {
+	return arr.map(e => e.slice());
+}
+
+async function wait(seconds) {
+	return new Promise((resolve, reject) => setTimeout(resolve, seconds * 1000));
+}
+async function waitFrame() {
+	return new Promise((resolve, reject) => requestAnimationFrame(resolve));
+}
+
+async function playAnimation(queens, log) {
+	console.log("animation");
+	for(let step=0; step<log.length; step++) {
+		let q = log[step][0];
+		let oldx = queens[q][0];
+		let oldy = queens[q][1];
+		let newx = log[step][1];
+		let newy = log[step][2];
+		for(let t=0; t<=1; t+=0.01) {
+			let t2 = t * t * (1.5 - t) * 2;
+			queens[q][0] = oldx + (newx - oldx) * t2;
+			queens[q][1] = oldy + (newy - oldy) * t2 - t * (1-t);
+			draw(queens);
+			await waitFrame();
+		}
+	}
+}
---
+++ Lab2/LDFS/main.js
@@ -0,0 +1,242 @@
+const canvas = document.getElementById("canv");
+const ctx = canvas.getContext("2d");
+const queen = new Image(); queen.src = "../queen.png";
+
+const queens2 = [];
+const playfield = new Array(8).fill(0).map(array => new Array(8).fill(0));
+const log = [];
+
+let maxDepth = 6;
+let totalWrong = 0;
+let queenCount = 8;
+let fieldSize = 8;
+let delay = 0;
+
+document.getElementById("start").addEventListener("click", async function() {
+	queenCount = Math.max(document.getElementById("queen").value | 0, 2);
+	fieldSize = Math.max(document.getElementById("board").value | 0, 2);
+	maxDepth = Math.max(document.getElementById("depth").value | 0, 1);
+	delay = Math.max(document.getElementById("delay").value | 0, 0);
+	canvas.width = canvas.height = 44 + fieldSize * 32;
+
+	addRandomly();
+	draw(queens2);
+	await wait(0.5);
+
+	document.getElementById("info").innerText = "Пошук шляху...";
+	let startTime = Date.now();
+
+	try {
+		let queens = copy(queens2);
+		for(let q=0; q<queenCount; q++) {
+			let x = queens[q][0];
+			let y = queens[q][1];
+			for(let i=0; i<fieldSize; i++) {
+				if(x !== i) await move(queens, q, i, y, 0, 1);
+			}
+			for(let i=0; i<fieldSize; i++) {
+				if(y !== i) await move(queens, q, x, i, 0, 2);
+			}
+			let z = y-x;
+			for(let i=0; i<fieldSize; i++) {
+				if(x !== i && z+i >= 0 && z+i < fieldSize) await move(queens, q, i, z+i, 0, 3);
+			}
+			z = y+x;
+			for(let i=0; i<fieldSize; i++) {
+				if(x !== i && z-i >= 0 && z-i < fieldSize) await move(queens, q, i, z-i, 0, 4);
+			}
+		}
+	} catch(e) {
+		console.log("errored", e);
+	}
+	document.getElementById("info").innerText = "Шлях знайдено за "+((Date.now() - startTime) / 1000)+" секунд";
+	await playAnimation();
+});
+
+function draw(queens) {
+	queens = queens.slice().sort((a,b) => a[1]-b[1]);
+	ctx.clearRect(0, 0, 300, 300);
+
+	ctx.fillStyle = "black";
+	ctx.fillRect(20, 20, fieldSize*32+4, fieldSize*32+4);
+	ctx.font = "20px sans-serif";
+
+	const colors = ["white", "black"];
+	for(let y=0; y<fieldSize; y++) {
+		for(let x=0; x<fieldSize; x++) {
+			ctx.fillStyle = colors[(x+y) % 2];
+			ctx.fillRect(22+x*32, 22+y*32, 32, 32);
+		}
+	}
+	for(let q=0; q<queenCount; q++) {
+		let x = queens[q][0];
+		let y = queens[q][1];
+		ctx.drawImage(queen, 22+x*32, 22+y*32);
+		if(delay > 0) {
+			let l = ctx.measureText(queens[q][2]).width/2;
+			ctx.fillStyle = "#000000";
+			ctx.fillText(queens[q][2], 38-l+x*32, 46+y*32);
+			ctx.fillStyle = "#ff0000";
+			ctx.fillText(queens[q][2], 38-l+x*32, 44+y*32);
+		}
+	}
+	ctx.fillStyle = "black";
+	let fs32 = fieldSize*32;
+	for(let y=0; y<fieldSize; y++) {
+		ctx.fillText("12345678"[y], 4, 12+fs32-y*32);
+		ctx.fillText("12345678"[y], 31+fs32, 12+fs32-y*32);
+		ctx.fillText("ABCDEFGH"[y], 33+y*32, 14);
+		ctx.fillText("ABCDEFGH"[y], 33+y*32, 44+fs32);
+	}
+}
+
+
+function addRandomly() {
+	let add = queenCount;
+	while(add > 0) {
+		let x = random(0, fieldSize-1);
+		let y = random(0, fieldSize-1);
+		let skip = false;
+		for(let q=0; q<queens2.length; q++) {
+			if(queens2[q][0] === x && queens2[q][1] === y) skip = true;
+		}
+		if(skip) continue;
+		playfield[y][x] = 1;
+
+		let wrong = 0;
+		for(let i=0; i<queens2.length; i++) {
+			let x2 = queens2[i][0];
+			let y2 = queens2[i][1];
+			if(x === x2 || y === y2 || x+y === x2+y2 || x-y === x2-y2) {
+				queens2[i][2]++;
+				wrong++;
+				totalWrong++;
+			}
+		}
+		totalWrong += wrong+1;
+		queens2.push([x, y, wrong+1]);
+		console.log(x, y);
+		add--;
+	}
+}
+
+function random(a, b) {
+	return a + Math.floor(Math.random() * (b-a+1));
+}
+
+async function move(queens, queen, x, y, depth, last) {
+	if(depth > maxDepth) return;
+
+	if(playfield[y][x] === 1) return;
+	let oldx = queens[queen][0];
+	let oldy = queens[queen][1];
+	let oldw = queens[queen][2];
+	log.push([queen, x, y]);
+	queens[queen][0] = x;
+	queens[queen][1] = y;
+	playfield[y][x] = 1;
+	playfield[oldy][oldx] = 0;
+
+	totalWrong -= oldw;
+	let wrong = 0;
+	for(let i=0; i<queenCount; i++) {
+		if(i == queen) continue;
+		let x2 = queens[i][0];
+		let y2 = queens[i][1];
+		if(oldx === x2 || oldy === y2 || oldx+oldy === x2+y2 || oldx-oldy === x2-y2) {
+			queens[i][2]--;
+			totalWrong--;
+		}
+		if(x === x2 || y === y2 || x+y === x2+y2 || x-y === x2-y2) {
+			queens[i][2]++;
+			wrong++;
+			totalWrong++;
+		}
+	}
+	queens[queen][2] = wrong+1;
+	totalWrong += wrong+1;
+	if(totalWrong == queenCount) throw new Error("SOLVED!!!")
+
+	if(delay > 0) {
+		draw(queens);
+		await wait(delay);
+	}
+
+	depth++;
+	for(let q=0; q<queenCount; q++) {
+		let x = queens[q][0];
+		let y = queens[q][1];
+		if(q !== queen || last !== 1) {
+			for(let i=0; i<fieldSize; i++) {
+				if(x !== i) await move(queens, q, i, y, depth, 1);
+			}
+		}
+		if(q !== queen || last !== 2) {
+			for(let i=0; i<fieldSize; i++) {
+				if(y !== i) await move(queens, q, x, i, depth, 2);
+			}
+		}
+		if(q !== queen || last !== 3) {
+			let z = y-x;
+			for(let i=0; i<fieldSize; i++) {
+				if(x !== i && z+i >= 0 && z+i < fieldSize) await move(queens, q, i, z+i, depth, 3);
+			}
+		}
+		if(q !== queen || last !== 4) {
+			let z = y+x;
+			for(let i=0; i<fieldSize; i++) {
+				if(x !== i && z-i >= 0 && z-i < fieldSize) await move(queens, q, i, z-i, depth, 4);
+			}
+		}
+	}
+	queens[queen][0] = oldx;
+	queens[queen][1] = oldy;
+	queens[queen][2] = oldw;
+	playfield[y][x] = 0;
+	playfield[oldy][oldx] = 1;
+	totalWrong -= wrong+1;
+	totalWrong += oldw;
+	for(let i=0; i<queenCount; i++) {
+		if(i == queen) continue;
+		let x2 = queens[i][0];
+		let y2 = queens[i][1];
+		if(oldx === x2 || oldy === y2 || oldx+oldy === x2+y2 || oldx-oldy === x2-y2) {
+			queens[i][2]++;
+			totalWrong++;
+		}
+		if(x === x2 || y === y2 || x+y === x2+y2 || x-y === x2-y2) {
+			queens[i][2]--;
+			totalWrong--;
+		}
+	}
+	log.pop();
+}
+
+function copy(arr) {
+	return arr.map(e => e.slice());
+}
+
+async function wait(seconds) {
+	return new Promise((resolve, reject) => setTimeout(resolve, seconds * 1000));
+}
+async function waitFrame() {
+	return new Promise((resolve, reject) => requestAnimationFrame(resolve));
+}
+
+async function playAnimation() {
+	console.log("animation");
+	for(let step=1; step<log.length; step++) {
+		let q = log[step][0];
+		let oldx = queens2[q][0];
+		let oldy = queens2[q][1];
+		let newx = log[step][1];
+		let newy = log[step][2];
+		for(let t=0; t<=1; t+=0.01) {
+			let t2 = t * t * (1.5 - t) * 2;
+			queens2[q][0] = oldx + (newx - oldx) * t2;
+			queens2[q][1] = oldy + (newy - oldy) * t2 - t * (1-t);
+			draw(queens2);
+			await waitFrame();
+		}
+	}
+}
